46702773389daf8472bd340bac52882963555b3d
gaelleEK <elkadaouigaelle@gmail.com>
2021-02-24 15:21:01 +0100

    add navbar

--- wp-content/themes/mytheme/single-post.php
+++ wp-content/themes/mytheme/single-post.php
@@ -4,10 +4,12 @@
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
         <h1><?php the_title() ?></h1>
+<!--    -->
 <?php //the_post_thumbnail() ?><!--      peut etre remplacer par  -->
         <p>
-            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title() ?>">
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title() ?>" style="width: 100%; height: auto;">
         </p>
+<!--    -->
         <?php the_content() ?>
 
     <?php endwhile; endif; ?>
