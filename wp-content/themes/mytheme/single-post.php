aedef66ae11ac009b50f0a99a5260509ae48aa33
gaelleEK <elkadaouigaelle@gmail.com>
2021-02-26 10:28:33 +0100

    add add 'articles récents' single-post

--- wp-content/themes/mytheme/single-post.php
+++ wp-content/themes/mytheme/single-post.php
@@ -1,23 +1,64 @@
 
 <?php get_header() ?>
 
-    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+<?php if (have_posts()): while (have_posts()): the_post(); ?>
 
-        <h1><?php the_title() ?></h1>
+    <h1><?php the_title() ?></h1>
 
-        <?php if (get_post_meta(get_the_ID(), SponsoMetaBox::META_KEY, true) == '1'): ?>
-            <div class="alert alert-info">
-                Cet article est sponsorisé
+    <?php if (get_post_meta(get_the_ID(), SponsoMetaBox::META_KEY, true) == '1'): ?>
+        <div class="alert alert-info">
+            Cet article est sponsorisé
+        </div>
+    <?php endif; ?>
+    <!--    -->
+    <?php //the_post_thumbnail() ?><!--      peut etre remplacer par  -->
+    <p>
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title() ?>" style="width: 100%; height: auto;">
+    </p>
+    <!--    -->
+
+    <?php the_content() ?>
+
+    <h2>Articles relatifs</h2>
+
+    <div class="row">
+        <?php
+        $sports = array_map(function (WP_Term $term) {
+            return $term->term_id;
+        }, get_the_terms(get_post(), 'sport'));
+        $query = new WP_Query([
+            'post__not_in' => [get_the_ID()],
+            'post_type' => 'post',
+            'posts_per_page' => 3,
+            'orderby' => 'rand',
+            'tax_query' => [
+                [
+                    'taxonomy' => 'sport',
+                    'terms' => $sports,
+                ]
+            ],
+            /* condition sur les meta de l article
+            **** Recherche tous les articles sponsorisés
+            'meta_query' => [
+                [
+                    'key' => SponsoMetaBox::META_KEY,
+                    'compare' => 'EXISTS'
+                ]
+            ]
+            */
+        ]);
+        while ($query->have_posts()): $query->the_post();
+            ?>
+
+            <div class="col-sm-4">
+                <?php get_template_part('parts/card', 'post'); ?>
             </div>
-        <?php endif; ?>
-<!--    -->
-        <?php //the_post_thumbnail() ?><!--      peut etre remplacer par  -->
-        <p>
-            <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title() ?>" style="width: 100%; height: auto;">
-        </p>
-<!--    -->
+        <?php
+        endwhile;
+        wp_reset_postdata();
+        ?>
+    </div>
 
-        <?php the_content() ?>
+<?php endwhile; endif; ?>
 
-    <?php endwhile; endif; ?>
 <?php get_footer() ?>
