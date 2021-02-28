ee5aa418f7a583e317490450c561b6e4d37daf8d
gaelleEK <elkadaouigaelle@gmail.com>
2021-02-28 21:41:26 +0100

    add widget youtube

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -4,7 +4,10 @@
             'theme_location' => 'footer',
             'container' => false,
             'menu_class' => 'navbar-nav mr-auto'
-        ]) ?>
+        ]);
+        // test widget autre facon de procéder
+        the_widget(YoutubeWidget::class, ['title' => 'salut', 'youtube' => 'X2kPLQEFR6Y']);
+        ?>
     </footer>
     <div>
         <?= get_option('agence_horaire') ?>
--- wp-content/themes/mytheme/front-page.php
+++ wp-content/themes/mytheme/front-page.php
@@ -1,19 +1,19 @@
 <?php get_header() ?>
 <?php custom_banners_register_required_plugins() ?>
+<div class="row">
+    <div class="col-md-8"
+    <?php while (have_posts()): the_post() ?>
 
+        <h1><?php the_title() ?></h1>
 
-<aside class="col-md-4 blog-sidebar">
-    <?= get_sidebar('homepage'); ?>
-</aside>
+        <?php the_content(); ?>
 
-<?php while (have_posts()): the_post() ?>
+        <a href="<?= get_post_type_archive_link('post') ?>">Voir les dernières actualités</a>
 
-    <h1><?php the_title() ?></h1>
-
-    <?php the_content(); ?>
-
-    <a href="<?= get_post_type_archive_link('post') ?>">Voir les dernières actualités</a>
-
-<?php endwhile; ?>
-
+    <?php endwhile; ?>
+    </div>
+    <aside class="col-md-4 blog-sidebar">
+        <?= get_sidebar('homepage'); ?>
+    </aside>
+</div>
 <?php get_footer() ?>
